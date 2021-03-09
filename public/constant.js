a0d761b2784c3068a9604152ae4644d07bb2ce3b
waleed jawaid <waleed.jawaid@salsoft.biz>
2021-03-10 02:51:11 +0500

    relative url added

--- public/constant.js
+++ public/constant.js
@@ -1,4 +1,4 @@
 //export const BaseUrl = "https://jsonplaceholder.typicode.com/todos";
 export const BaseUrl = "http://13.52.138.170/api";
-export const ImageUrl = "http://13.52.138.170/public/graph";
+export const ImageUrl = "//13.52.138.170/public/graph";
 
